aae133cbc8ca96637eb771c16e5bed02647df553
Fumiya Kito <cfbc.pearl@gmail.com>
2021-02-22 23:32:56 +0900

    create frontend

---
+++ frontend/src/componets/Footer.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function Footer() { 
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">Copyright &copy; Fumi's Blog</Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Footer
